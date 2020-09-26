9f5b9d3fc4bc47b1841528eff93d904e80a154e0
Sara Bastian <seb118@georgetown.edu>
2020-09-26 19:30:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -114,5 +114,16 @@ def game_hash(hash)
         :blocks => 1,
         :slam_dunks => 0
         },
+        {
+        :player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
+        },
         ], 
   }
