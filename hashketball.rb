7340a25ed3c418e042d34f54c36bf0018438ed5b
Sara Bastian <seb118@georgetown.edu>
2020-09-26 19:27:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,6 +79,17 @@ def game_hash(hash)
         :assists => 1,
         :steals => 2,
         :blocks => 7,
+        :slam_dunks => 2 
+        }
+         {
+        :player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
         :slam_dunks => 2}
         ], 
   }
