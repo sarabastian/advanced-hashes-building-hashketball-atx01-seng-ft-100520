250e6c766951b2a45a44d739c7d8ca697be71549
Sara Bastian <seb118@georgetown.edu>
2020-09-26 19:53:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,8 +68,8 @@ def game_hash()
     :away => {
       
       :team_name => "Charlotte Hornets",
-      :colors = ["Turquoise", "Purple"],
-      :players = [ 
+      :colors => ["Turquoise", "Purple"],
+      :players => [ 
         {
         :player_name => "Jeff Adrien",
         :number => 4,
